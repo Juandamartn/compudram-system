9b6d9fadcf1c4d6c9d50bd913be5d386481d9fa2
juan <mtzn_juandaniel@yahoo.com.mx>
2020-06-11 17:08:36 -0600

    Creating relationships in Models to work with DB data

--- app/Client.php
+++ app/Client.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    /**
+     * The roles that belong to the user.
+     */
+    public function licenses()
+    {
+        return $this->hasMany(License::class, 'client_id');
+    }
 }
--- app/License.php
+++ app/License.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class License extends Model
 {
-    //
+    /**
+     * The roles that belong to the user.
+     */
+    public function client()
+    {
+        return $this->belongsTo(Client::class, 'client_id');
+    }
+
+    /**
+     * The roles that belong to the user.
+     */
+    public function system()
+    {
+        return $this->belongsTo(System::class, 'system_id');
+    }
 }
