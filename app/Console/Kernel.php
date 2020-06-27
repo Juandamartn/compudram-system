e9185a7de4f2f19c3328d072b428a9bc11f9eab7
juan <mtzn_juandaniel@yahoo.com.mx>
2020-06-26 22:34:52 -0600

    Changed interval for commands in schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('license:expire')->dailyAt('00:45');
+        $schedule->command('license:expire')->everyFiveMinutes();
     }
 
     /**
